d1ecd6ce7bc408690e42dab79622e8ea6469301e
Blend Ibraj <blendi.ibraj96@gmail.com>
2020-04-19 19:19:59 +0000

    created get_games and list_games methods to make an array of games and then list them in numbered order

--- lib/top_open_world_games/cli.rb
+++ lib/top_open_world_games/cli.rb
@@ -1,6 +1,20 @@
 class TopOpenWorldGames::CLI
   def call
-    puts "Welcome! Here are the top 5 open world video games of all time!"
-    binding.pry
+    puts ""
+    puts "Welcome! Here are Glitchwave's top 5 open world video games of all time!"
+    puts ""
+    get_games
+    list_games
+  end
+  
+  def get_games
+    @games = ["LoZ BOTW", "Blood and Wine", "The Witcher 3", "Yakuza 0", "Fallout 2"]
+  end
+  
+  def list_games
+    puts "Select a game (1-5) to see more information!"
+    @games.each.with_index(1) do |game, index|
+      puts "#{index}. #{game}"
+    end
   end
 end
