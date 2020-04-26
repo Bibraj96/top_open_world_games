1ecfc11fd5c0bb639931cacefb12665c0491e98c
Blend Ibraj <blendi.ibraj96@gmail.com>
2020-04-26 18:23:48 +0000

    saved a commented out error that appears randomly when trying to scrape.

--- lib/top_open_world_games/cli.rb
+++ lib/top_open_world_games/cli.rb
@@ -43,7 +43,7 @@ class TopOpenWorldGames::CLI
   
   def continue
     puts "***************************************"
-    puts "To choose another game, type any key."
+    puts "To choose another game, type any key to display the top 40."
     puts "To exit, type 'exit'"
     @input = gets.strip
   end 
@@ -52,6 +52,21 @@ class TopOpenWorldGames::CLI
     puts "Have fun exploring your open world game!"
   end
 end
+
+#Traceback (most recent call last):
+#       12: from ./bin/top_open_world_games:5:in `<main>'
+#        11: from /home/Bibraj96/Development/top_open_world_games/lib/top_open_world_games/cli.rb:6:in `call'
+#        10: from /home/Bibraj96/Development/top_open_world_games/lib/top_open_world_games/cli.rb:15:in `get_games'
+#        9: from /home/Bibraj96/Development/top_open_world_games/lib/top_open_world_games/game.rb:15:in `all'
+#         8: from /home/Bibraj96/Development/top_open_world_games/lib/top_open_world_games/scraper.rb:4:in `scrape_games'
+#         7: from /usr/local/rvm/rubies/ruby-2.6.1/lib/ruby/2.6.0/open-uri.rb:35:in `open'
+#         6: from /usr/local/rvm/rubies/ruby-2.6.1/lib/ruby/2.6.0/open-uri.rb:736:in `open'
+#         5: from /usr/local/rvm/rubies/ruby-2.6.1/lib/ruby/2.6.0/open-uri.rb:165:in `open_uri'
+#         4: from /usr/local/rvm/rubies/ruby-2.6.1/lib/ruby/2.6.0/open-uri.rb:224:in `open_loop'
+#         3: from /usr/local/rvm/rubies/ruby-2.6.1/lib/ruby/2.6.0/open-uri.rb:224:in `catch'
+#         2: from /usr/local/rvm/rubies/ruby-2.6.1/lib/ruby/2.6.0/open-uri.rb:226:in `block in open_loop'
+#         1: from /usr/local/rvm/rubies/ruby-2.6.1/lib/ruby/2.6.0/open-uri.rb:756:in `buffer_open'
+#/usr/local/rvm/rubies/ruby-2.6.1/lib/ruby/2.6.0/open-uri.rb:378:in `open_http':503 Service Unavailable (OpenURI::HTTPError)
 
 
 
